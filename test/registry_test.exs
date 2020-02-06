26fce04b3c587dc668e045864d7d20f3aff75075
Anderson Mesquita <andersonm@spotify.com>
2020-02-06 11:31:10 -0500

    Extract bucket name as constant

--- test/registry_test.exs
+++ test/registry_test.exs
@@ -1,16 +1,18 @@
 defmodule KV.RegistryTest do
   use ExUnit.Case, async: true
 
+  @bucket_name "shopping"
+
   setup do
     registry = start_supervised!(KV.Registry)
     %{registry: registry}
   end
 
   test "spawns buckets", %{registry: registry} do
-    assert KV.Registry.lookup(registry, "shopping") == :error
+    assert KV.Registry.lookup(registry, @bucket_name) == :error
 
-    KV.Registry.create(registry, "shopping")
+    KV.Registry.create(registry, @bucket_name)
 
-    assert {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+    assert {:ok, bucket} = KV.Registry.lookup(registry, @bucket_name)
   end
 end
